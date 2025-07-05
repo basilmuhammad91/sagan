2b4ca941447a2aaf72a842ef4fe93314c9369c12
basilmuhammad91 <basilmuhammad91@gmail.com>
2025-07-05 15:09:00 +0500

    =auth installations

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,11 @@
 import './bootstrap';
 
 import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/vue3'
+import { createInertiaApp } from '@inertiajs/inertia-vue3'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 
 createInertiaApp({
-  resolve: name => require(`./Pages/${name}.vue`),
+  resolve: name => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
