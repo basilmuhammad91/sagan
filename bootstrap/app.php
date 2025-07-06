b29d12f0d7b7320afa2b316990c60f9bb679dba2
basilmuhammad91 <basilmuhammad91@gmail.com>
2025-07-06 14:45:18 +0500

    fixed

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->web(append: [
+        $middleware->alias([
+            'guest' => \App\Http\Middleware\RedirectIfAuthenticatedCustom::class,
+        ])
+        ->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
         ]);
     })
