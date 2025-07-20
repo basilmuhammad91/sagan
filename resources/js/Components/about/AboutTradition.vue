87d43c14fe3a0f2c05a607afce456db42cebad16
basilmuhammad91 <basilmuhammad91@gmail.com>
2025-07-20 17:37:16 +0500

    working on about

---
+++ resources/js/Components/about/AboutTradition.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="py-16 bg-white">
+    <div class="container flex flex-col items-center gap-10 px-6 mx-auto md:px-12 md:flex-row">
+      <!-- Left Text Content -->
+      <div class="text-center md:w-1/2 md:text-left">
+        <h4 class="mb-2 font-semibold tracking-widest text-[#272727] uppercase text-md">
+          About Us
+        </h4>
+        <h2 class="mb-4 text-3xl font-extrabold leading-tight text-[#272727] home-heading md:text-4xl">
+          Honoring traditions, <br class="hidden md:block" />
+          <span class="text-gray-800">digitally.</span>
+        </h2>
+        <p class="font-semi-bold font-space-grotesk max-w-md mx-auto leading-relaxed text-[#272727] md:mx-0">
+          At SAGANonline, we believe cash gifts are more than just money—they’re a gesture of love, generosity, and good fortune. Across cultures, gifting cash has been a time-honored tradition, from wedding contributions and baby blessings to group gifts and milestone celebrations.
+        </p>
+      </div>
+
+      <!-- Right Image Content -->
+      <div class="relative flex justify-center md:w-1/2">
+        <!-- Actual Image -->
+        <img
+          src="/assets/images/about.jpg"
+          alt="Gift Tradition"
+          class="relative w-[500px] h-[400px] object-cover rounded-xl z-10"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AboutTradition',
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+</style>
--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -1,13 +1,24 @@
 <template>
-    <h1>
-        Welcome to About Us
-    </h1>
+    <div>
+        <AboutTradition />
+        <AboutEvent />
+        <TrustedSection />
+
+    </div>
 </template>
 
 <script>
 import AppLayout from '@/Layouts/AppLayout.vue'
+import AboutTradition from '@/Components/about/AboutTradition.vue'
+import TrustedSection from '@/Components/home/TrustedSection.vue'
+import AboutEvent from '@/Components/about/AboutEvent.vue'
 
 export default {
-  layout: AppLayout
+  layout: AppLayout,
+    components: {
+        AboutTradition,
+        TrustedSection,
+        AboutEvent
+    },
 }
 </script>
