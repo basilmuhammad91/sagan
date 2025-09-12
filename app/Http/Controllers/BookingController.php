e1b93dcec85051b9b76c5f8d5fa0863943148587
basil-user <basil@asaanretail.io>
2025-09-12 23:19:32 +0500

    fixing more issues

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -1,8 +1,7 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use App\Http\Requests\BookingRequest;
 use App\Http\Resources\BookingResource;
 use App\Models\Booking;
